ed54e9c407f44a593f9affb95cee7f738ce3a8bd
Vadzim Tatyrzha <vadikTatyrzha@gmail.com>
2024-12-05 00:24:16 +0300

    feat: create GetBooks in BookService

---
+++ TestTask/Services/Implementations/BookService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class BookService : IBookService
+    {
+        private readonly ApplicationDbContext _context;
+        public BookService(ApplicationDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+        public async Task<Book> GetBook()
+        {
+            //var costOfPublishedEdition = 
+            return await _context.Books.OrderBy(b => b.QuantityPublished * b.Price).ElementAtOrDefaultAsync(1);
+        }
+        public async Task<List<Book>> GetBooks()
+        {
+            // Carolus Rex release date — 2012 may 25.
+            var carolusRexReleaseDate = new DateTime(2012, 05, 25);
+            var keyWord = "red";
+            return await _context.Books.Where(b => b.PublishDate > carolusRexReleaseDate && b.Title.ToLower().Contains(keyWord)).ToListAsync();
+        }
+    }
+}
